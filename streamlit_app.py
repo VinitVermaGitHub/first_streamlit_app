7d08e63611b20639f6f4e10c898d06fb0c8f223a
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-06 16:37:05 -0700

    Update streamlit_app.py
    Added new text and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Classic: two scrambled eggs with smoked apple sausages and toast')
 streamlit.text('Mediterranean omlette with three eggs, feta cheese, olives, and spinach')
 streamlit.text('Mexican omlette with three eggs, refried beans, avacado, and tabasco sauce')
+streamlit.text('Avacado toast')
+streamlit.header('🍎🍐 Build your own fruit smoothies 🍉🍇')

ae93f844001c7d1612b7e3b43bcf961e640e0ec6
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-06 16:32:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,3 @@ streamlit.header('Breakfast menu')
 streamlit.text('Classic: two scrambled eggs with smoked apple sausages and toast')
 streamlit.text('Mediterranean omlette with three eggs, feta cheese, olives, and spinach')
 streamlit.text('Mexican omlette with three eggs, refried beans, avacado, and tabasco sauce')
-

e07eb52eec760def675abd4cbd6aabd9ca8dd8ee
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-17 14:27:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text('🥪 toast')
 streamlit.header('🍎🍐 Build your own fruit smoothies 🍉🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 
 #display the table on the page

616a44c4087a4a557c103a6dc33fb7f25fe38929
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-17 14:11:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,3 @@ streamlit.header('🍎🍐 Build your own fruit smoothies 🍉🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-streamlit.text('what is going on')

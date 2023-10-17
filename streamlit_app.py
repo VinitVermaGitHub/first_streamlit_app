a8c70a7fb2fa2f9b96985311326e3db47b45bb82
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-17 16:10:35 -0700

    Update streamlit_app.py
    display only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,7 @@ streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
+#display only selected fruit
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected])
+streamlist.dataframe(fruits_to_show)

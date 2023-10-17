eacb087063ead61b4743de3963d5bd6909355dd9
VinitVermaGitHub <147084798+VinitVermaGitHub@users.noreply.github.com>
2023-10-17 13:22:57 -0700

    Update streamlit_app.py
    Added emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast menu')
-streamlit.text('Classic: two scrambled eggs with smoked apple sausages and toast')
-streamlit.text('Mediterranean omlette with three eggs, feta cheese, olives, and spinach')
-streamlit.text('Mexican omlette with three eggs, refried beans, avacado, and tabasco sauce')
-streamlit.text('Avacado toast')
+streamlit.text('🫕 Classic: two scrambled eggs with smoked apple sausages and toast')
+streamlit.text('🥬 omlette with three eggs, feta cheese, olives, and spinach')
+streamlit.text('🫒 Mexican omlette with three eggs, refried beans, avacado, and tabasco sauce')
+streamlit.text('🥪 toast')
 streamlit.header('🍎🍐 Build your own fruit smoothies 🍉🍇')
